5cf5efb78d87b1efc9ea53c4efc9a42c7e08a9a3
Ashish <savitayadavmansarover@gmail.com>
2023-07-29 22:27:12 +0530

    updated with scrollTrigger

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 
 const tl = gsap.timeline();
+
+tl.from("#navbar", {
+    opacity: 0,
+    duration: 0.5,
+})
 tl.from("#navbar img, #navbar h1,#navbar button", {
     y: -100,
-    opacity: 0,
-    duration: 1,
-    opacity: 0,
+    duration: 0.5,
     stagger: 0.3,
 })
 tl.from("#content-box1 img", {
@@ -13,15 +16,27 @@ tl.from("#content-box1 img", {
     opacity: 0,
     duration: 0.6,
 })
+
 tl.from("#content-box2", {
     opacity: 0,
     duration: 0.7,
 })
-
-
 tl.from(".horizon-1 , .data, #headingcb3", {
     opacity: 0,
     scale: 1.5,
     stagger: 0.2,
 })
-tl.from(".horizon-1 img", { rotate: 360, scale: 0.5 })
+tl.from(".horizon-1 img", { rotate: 360, scale: 0.5, duration: 0.5 });
+gsap.from(".page2-boxes", {
+    scale: 0,
+    duration: 0.5,
+    stagger: 0.3,
+    scrollTrigger: {
+        trigger: ".pages2-boxes",
+        scroller: "body",
+        markers: true,
+        start: '70% 30%', //start setstart
+        end: 'center center',
+        scrub: 4,
+    }
+})
